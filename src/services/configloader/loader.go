e319fa91154c84c11cf5a4b55886c55c2fdae6ef
bdoerfchen <93781550+bdoerfchen@users.noreply.github.com>
2025-08-19 11:45:33 +0200

    feat: yaml config support

--- src/services/configloader/loader.go
+++ src/services/configloader/loader.go
@@ -2,15 +2,16 @@ package configloader
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/bdoerfchen/webcmd/src/logging"
 	"github.com/bdoerfchen/webcmd/src/model/config"
 	"github.com/jinzhu/copier"
+	"sigs.k8s.io/yaml"
 )
 
 const DefaultConfigFile = "server.conf"
@@ -33,6 +34,15 @@ func (l *configLoader) Load(ctx context.Context, path string) (*config.AppConfig
 		useDefaultPath = true
 	}
 
+	file, err := os.Stat(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file: %w", err)
+	}
+	// Append default file name if provided path is a directory
+	if file.IsDir() {
+		path = filepath.Join(path, DefaultConfigFile)
+	}
+
 	// Read file
 	content, err := os.ReadFile(path)
 	if err != nil {
@@ -44,33 +54,23 @@ func (l *configLoader) Load(ctx context.Context, path string) (*config.AppConfig
 		return &result, nil
 	}
 
-	// Try all unmarshallers
-	for _, u := range unmarshallers {
-		logger.Debug("trying format '" + u.Name() + "'")
-
-		output, err := u.Unmarshal(content)
-		if err != nil {
-			if errors.Is(err, errTypeNotCorrect) {
-				continue
-			}
-
-			return nil, fmt.Errorf("could not read config file: %w", err)
-		}
-
-		// Go over read config and validate
-		result := config.DefaultAppConfig()
-		copier.CopyWithOption(&result.Server, &output.Server, copier.Option{IgnoreEmpty: true})
-		for _, configRoute := range output.Routes {
-			copiedRoute := config.DefaultRoute()
-			copier.CopyWithOption(&copiedRoute, &configRoute, copier.Option{IgnoreEmpty: true})
-			copiedRoute.StatusCodes = append(config.DefaultRoute().StatusCodes, configRoute.StatusCodes...)
-			result.Routes = append(result.Routes, copiedRoute)
-		}
-
-		return &result, nil
+	var parsedConfig config.AppConfig
+	err = yaml.Unmarshal(content, &parsedConfig)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config file: %w", err)
 	}
 
-	return nil, errFormatUnknown
+	// Use default config as base and overlay parsed server config onto it
+	copier.CopyWithOption(&result.Server, &parsedConfig.Server, copier.Option{IgnoreEmpty: true})
+	for _, configRoute := range parsedConfig.Routes {
+		// Go over parsed config's routes and append their content overlayed on the default to the result config
+		copiedRoute := config.DefaultRoute()
+		copier.CopyWithOption(&copiedRoute, &configRoute, copier.Option{IgnoreEmpty: true})
+		copiedRoute.StatusCodes = append(config.DefaultRoute().StatusCodes, configRoute.StatusCodes...)
+		result.Routes = append(result.Routes, copiedRoute)
+	}
+
+	return &result, nil
 }
 
 func AppendRoute(appConfig *config.AppConfig, route *config.Route, logger *slog.Logger) {
